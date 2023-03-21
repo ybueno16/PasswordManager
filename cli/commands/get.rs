c241a77b93573eadb43c3d02ca8f2e153ef3a33b
ybueno16 <yuribueno27@gmail.com>
2023-03-21 16:29:10 -0300

    Tweaks na formatação do código

--- cli/commands/get.rs
+++ cli/commands/get.rs
@@ -1,7 +1,6 @@
 use rusqlite::{ Connection, Result };
 
-pub fn get(conn: &Connection,id: i32, username: &str, password: &str, purpose: &str) {
+pub fn get(conn: &Connection, id: i32, username: &str, password: &str, purpose: &str) {
     crate::database::db::get_password_record(conn, id, username, password, purpose);
     println!("Select feito com sucesso");
-
 }
